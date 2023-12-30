4ee3d9310141b4c36a40a50d015a928a4ab0b0e7
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2023-12-30 09:18:32 +0300

    30.12 - lesson 1 - 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,37 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
+import {TodoList} from './TodoList'
+
 
 function App() {
+
+    const tasks1 = [
+        {id: 1, title: 'HTML&CSS', isDone: true},
+        {id: 2, title: 'JS', isDone: true},
+        {id: 3, title: 'ReactJS', isDone: false}
+    ]
+
+    const tasks2 = [
+        {id: 1, title: 'SC2', isDone: true},
+        {id: 2, title: 'APEX', isDone: false},
+        {id: 3, title: 'BG3', isDone: false}
+    ]
+
+    const tasks3 = [
+        {id: 1, title: 'Dark Tower', isDone: true},
+        {id: 2, title: 'Game og Thrones', isDone: false},
+        {id: 3, title: 'Witcher', isDone: true}
+    ]
+
+
     return (
         <div className="App">
-            <TodoList/>
-            <TodoList/>
-            <TodoList/>
-        </div>
-    );
-}
-
-
-const TodoList =()=>{
-    return(
-        <div>
-            <h3>What to learn</h3>
-            <div>
-                <input/>
-                <button>+</button>
-            </div>
-            <ul>
-                <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                <li><input type="checkbox" checked={false}/> <span>React</span></li>
-            </ul>
-            <div>
-                <button>All</button>
-                <button>Active</button>
-                <button>Completed</button>
-            </div>
+            <TodoList title={'What to learn'} tasks={tasks1}/>
+            <TodoList title={'Streams'} tasks={tasks2}/>
+            <TodoList title={'Books'} tasks={tasks3}/>
         </div>
     )
 }
 
 
-export default App;
+export default App
